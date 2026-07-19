ceec20086d5dbc6c34e8b43f9371d5951f2bb447
Flobul <46993341+Flobul@users.noreply.github.com>
2026-07-19 17:42:57 +0200

    Modification du champ de mot de passe pour afficher le texte au lieu de masquer les caractères

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -105,7 +105,7 @@
               <sup><i class="fas fa-question-circle" title="{{Entrez le mot de passe.}} {{Par défaut}} : cxswprin"></i></sup>
               </label>
               <div class="input-group col-sm-3">
-                <input type="password" class="inputPassword configKey form-control" data-l1key="password">
+                <input type="text" class="inputPassword configKey form-control" data-l1key="password">
                 <span class="input-group-btn">
                     <a class="btn btn-default form-control bt_showPass roundedRight"><i class="fas fa-eye"></i></a>
                 </span>
